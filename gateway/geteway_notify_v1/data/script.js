eb8fc75d0898dce22c945db0a885432106c7c897
jhaturaphat <umt.eng52@gmail.com>
2025-11-05 07:11:24 +0700

    test post json

--- gateway/geteway_notify_v1/data/script.js
+++ gateway/geteway_notify_v1/data/script.js
@@ -1,48 +1,92 @@
+function saveWificfg() {
+  const ssid = document.getElementById("wifi_ssid").value.trim();
+  const password = document.getElementById("wifi_password").value.trim();
+
+  fetch("/save_wifi", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ssid: ssid, password: password }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok: " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      alert("✅สำเร็จ!✅");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("⛔เกิดข้อผิดพลาด");
+    });
+}
+
+function saveNotifycfg() {
+  const radioButtons = document.querySelectorAll('input[name="notify"]');
+  radioButtons.forEach((radio) => {
+    if (this.checked) {
+      switch (this.id) {
+      }
+    }
+  });
+}
+
+function sendConfigNotify(playload) {  
+  fetch("/save_notify", {
+    method: "POST", 
+    headers: {      
+      "Content-Type": "application/json",
+    },    
+    body: JSON.stringify(playload),
+  })
+    .then((response) => {      
+      if (!response.ok) {        
+        throw new Error("Network response was not ok: " + response.statusText);
+      }      
+      return response.json();
+    })
+    .then((data) => {      
+      console.log("Success:", data);
+      alert("✅สำเร็จ!✅");    
+    })
+    .catch((error) => {      
+      console.error("Error:", error);
+      alert("⛔เกิดข้อผิดพลาด");    
+    });
+}
+
 const radioButtons = document.querySelectorAll('input[name="notify"]');
-      // วนลูปเพื่อเพิ่ม Event Listener ให้แต่ละตัว
-      radioButtons.forEach((radio) => {
-        radio.addEventListener("change", function () {
-          if (this.checked) {
-            console.log("ตัวเลือกที่ถูกเลือกคือ:", this.id);
-            console.log("Value คือ:", this.value);
-            switch (this.id) {
-              case "ntfy":
-                document.getElementsByClassName("ntfy")[0].style.display =
-                  "inline";
-                document.getElementsByClassName("telegram")[0].style.display =
-                  "none";
-                document.getElementsByClassName("discord")[0].style.display =
-                  "none";
-                document.getElementById("btnSaveNotify").style.display =
-                  "inline";
-                document.getElementById("notify-content").style.display =
-                  "block";
-                break;
-              case "telegram":
-                document.getElementsByClassName("ntfy")[0].style.display =
-                  "none";
-                document.getElementsByClassName("telegram")[0].style.display =
-                  "inline";
-                document.getElementsByClassName("discord")[0].style.display =
-                  "none";
-                document.getElementById("btnSaveNotify").style.display =
-                  "inline";
-                document.getElementById("notify-content").style.display =
-                  "block";
-                break;
-              case "discord":
-                document.getElementsByClassName("ntfy")[0].style.display =
-                  "none";
-                document.getElementsByClassName("telegram")[0].style.display =
-                  "none";
-                document.getElementsByClassName("discord")[0].style.display =
-                  "inline";
-                document.getElementById("btnSaveNotify").style.display =
-                  "inline";
-                document.getElementById("notify-content").style.display =
-                  "block";
-                break;
-            }
-          }
-        });
-      });
+radioButtons.forEach((radio) => {
+  radio.addEventListener("change", function () {
+    if (this.checked) {
+      switch (this.id) {
+        case "ntfy":
+          document.getElementsByClassName("ntfy")[0].style.display = "inline";
+          document.getElementsByClassName("telegram")[0].style.display = "none";
+          document.getElementsByClassName("discord")[0].style.display = "none";
+          document.getElementById("btnSaveNotify").style.display = "inline";
+          document.getElementById("notify-content").style.display = "block";
+          break;
+        case "telegram":
+          document.getElementsByClassName("ntfy")[0].style.display = "none";
+          document.getElementsByClassName("telegram")[0].style.display =
+            "inline";
+          document.getElementsByClassName("discord")[0].style.display = "none";
+          document.getElementById("btnSaveNotify").style.display = "inline";
+          document.getElementById("notify-content").style.display = "block";
+          break;
+        case "discord":
+          document.getElementsByClassName("ntfy")[0].style.display = "none";
+          document.getElementsByClassName("telegram")[0].style.display = "none";
+          document.getElementsByClassName("discord")[0].style.display =
+            "inline";
+          document.getElementById("btnSaveNotify").style.display = "inline";
+          document.getElementById("notify-content").style.display = "block";
+          break;
+      }
+    }
+  });
+});
